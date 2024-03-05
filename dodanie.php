a056b9676cd62df3e4bf5dab3fd56a083b17de4b
Tomasz <tomecki.tomasz@zs6sobieski.edu.pl>
2024-03-05 12:39:20 +0100

    Skończony egzamin

---
+++ dodanie.php
@@ -0,0 +1,11 @@
+<?php
+$con = mysqli_connect("localhost","root");
+mysqli_select_db($con,"ee09");
+@$nr=$_POST["nr"];
+@$r1=$_POST["r1"];
+@$r2=$_POST["r2"];
+@$r3=$_POST["r3"];
+$tresc="INSERT INTO ratownicy(nrKaretki,ratownik1,ratownik2,ratownik3) VALUES('$nr','$r1','$r2','$r3')";
+$zap= mysqli_query($con,$tresc);
+echo"Do bazy zostało wysłane zapytanie: ".$tresc;
+?>
